b614404a85dbdecba89e669b0fc808a4c084c84c
jenpl1451 <115338469+jenpl1451@users.noreply.github.com>
2023-05-24 09:52:41 +0300

    add_passport

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,20 @@
-<h1 class="text-3xl font-bold underline">
-  Hello world!
-</h1>
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  import { Passport } from "@credenza-web3/passport";
+
+
+  const passport = new Passport({
+    chainId: Passport.chains.POLYGON_MUMBAI,
+  });
+
+  onMount(async () => {
+    await passport.init();
+    passport.showNavigation();
+  });
+</script>
+
+<h1 class="text-3xl font-bold underline">Hello world!</h1>
 
 <style lang="postcss">
   :global(html) {
--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,8 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+
+import adapter from "@sveltejs/adapter-static";
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,12 +10,25 @@ const config = {
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
+	// kit: {
+	// 	// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+	// 	// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+	// 	// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+	// 	adapter: adapter()
+	// }
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+    inlineStyleThreshold: 8096,
+    prerender: {
+      crawl: false
+    },
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: 'index.html',
+      precompress: false,
+      strict: true
+    })
+  }
 };
 
 export default config;
