c2e3f4ebb14926548d5a47891b3d49eaea6d2a28
jenpl1451 <115338469+jenpl1451@users.noreply.github.com>
2023-05-25 12:12:33 +0300

    login

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,35 @@
-<script lang="ts">
-  import { onMount } from "svelte";
-
-  import { Passport } from "@credenza-web3/passport";
-
-
-  const passport = new Passport({
-    chainId: Passport.chains.POLYGON_MUMBAI,
-  });
-
-  onMount(async () => {
-    await passport.init();
-    passport.showNavigation();
-  });
-</script>
-
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
-
 <style lang="postcss">
   :global(html) {
     background-color: theme(colors.gray.100);
   }
 </style>
+
+
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  import { Passport } from "@credenza-web3/passport";
+
+  
+  const passport = new Passport({
+    chainId: Passport.chains.POLYGON_MUMBAI,
+  });
+
+  let isLogged = false;
+
+
+  onMount(async () => {
+    await passport.init();
+    passport.showNavigation();
+    console.log(passport);
+    passport.onLogin(()=> isLogged = true);
+    passport.onLogout(()=> isLogged = false);
+    isLogged = passport.isLoggedIn
+  });
+  
+</script>
+
+{#if !isLogged }
+<h1 class="text-3xl font-bold underline">Please login</h1>
+{/if}
+
